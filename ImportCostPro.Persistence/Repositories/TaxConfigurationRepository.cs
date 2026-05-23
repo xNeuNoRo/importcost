bce44d3632df6ae2ff0c17936d42ee43ed5212e8
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-05-22 23:28:41 -0400

    Implement ITaxConfigurationRepository and TaxConfigurationRepository for tax configuration management

---
+++ ImportCostPro.Persistence/Repositories/TaxConfigurationRepository.cs
@@ -0,0 +1,21 @@
+using ImportCostPro.Persistence.Contexts;
+using ImportCostPro.Persistence.Entities;
+using ImportCostPro.Persistence.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportCostPro.Persistence.Repositories
+{
+    public class TaxConfigurationRepository
+        : GenericRepository<TaxConfiguration>, // No importa q reutilicemos el crud,
+            // solo se expondra por la interfaz los metodos necesarios para esta entidad
+            ITaxConfigurationRepository
+    {
+        public TaxConfigurationRepository(AppDbContext context)
+            : base(context) { }
+
+        public async Task<TaxConfiguration?> GetSingleConfigurationAsync()
+        {
+            return await _dbSet.FirstOrDefaultAsync();
+        }
+    }
+}
