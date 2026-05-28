2a603b0dcdef478487aca7d9717003e44829c249
IsaiasMorillo <isaiasmorillo5556@gmail.com>
2026-05-27 23:01:39 -0400

    Add Currency view models: implement CurrencyCreateViewModel, CurrencyEditViewModel, and CurrencyViewModel with validation attributes

---
+++ ImportCostPro.Application/ViewModels/CurrencyViewModels/CurrencyUpdateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImportCostPro.WebApp.Models.Currency
+{
+    public class CurrencyEditViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la moneda es requerido.")]
+        [Display(Name = "Nombre de la moneda")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "El código ISO es requerido.")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "El código ISO debe tener exactamente 3 caracteres.")]
+        [Display(Name = "Código ISO")]
+        public string IsoCode { get; set; } = null!;
+
+        [Required(ErrorMessage = "El símbolo es requerido.")]
+        [Display(Name = "Símbolo")]
+        public string Symbol { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo Es moneda local es requerido.")]
+        [Display(Name = "Es moneda local")]
+        public bool IsLocalCurrency { get; set; }
+
+        [Required(ErrorMessage = "El estado es requerido.")]
+        [Display(Name = "Estado")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ImportCostPro.Application/ViewModels/CurrencyViewModels/CurrencyViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImportCostPro.WebApp.Models.Currency
+{
+    public class CurrencyViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre de la moneda")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Código ISO")]
+        public string IsoCode { get; set; } = null!;
+
+        [Display(Name = "Símbolo")]
+        public string Symbol { get; set; } = null!;
+
+        [Display(Name = "Moneda local")]
+        public bool IsLocalCurrency { get; set; }
+
+        [Display(Name = "Estado")]
+        public bool IsActive { get; set; }
+    }
+}
