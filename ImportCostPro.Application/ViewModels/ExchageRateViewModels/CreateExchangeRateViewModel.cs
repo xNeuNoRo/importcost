8045a359ed015b7ebd96da3b27fb26aa0ddad43d
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-05-29 03:33:56 -0400

    Add Create, Update, and View models for Exchange Rates with validation attributes

--- ImportCostPro.Application/ViewModels/ExchageRateViewModels/CreateExchangeRateViewModel.cs
+++ ImportCostPro.Application/ViewModels/ExchageRateViewModels/CreateExchangeRateViewModel.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ImportCostPro.Application.ViewModels.ExchageRateViewModels
 {
-    internal class CreateExchangeRateViewModel
+    public class CreateExchangeRateViewModel
     {
+        [Required(ErrorMessage = "La moneda origen seleccionada no es válida.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La moneda origen seleccionada no es válida.")]
+        [Display(Name = "Moneda Origen")]
+        public int FromCurrencyId { get; set; }
+
+        [Required(ErrorMessage = "La moneda destino seleccionada no es válida.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La moneda destino seleccionada no es válida.")]
+        [Display(Name = "Moneda Destino")]
+        public int ToCurrencyId { get; set; }
+
+        [Required(ErrorMessage = "El valor de la tasa es requerido.")]
+        [Range(0.0000000001, double.MaxValue, ErrorMessage = "El valor de la tasa debe ser mayor que 0.")]
+        [Display(Name = "Valor de la Tasa")]
+        public decimal RateValue { get; set; }
+
+        [Required(ErrorMessage = "La fecha de vigencia es requerida.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Vigencia")]
+        public DateTime EffectiveDate { get; set; } = DateTime.Today;
     }
 }
