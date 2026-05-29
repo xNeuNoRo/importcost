32e8f6c3e435fbd33a888e624bc1e42c8948f82a
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-05-29 03:54:55 -0400

    Add Create, Update, and View models for Order Products with validation attributes

--- ImportCostPro.Application/ViewModels/OrderProductViewModels/OrderProductViewModel.cs
+++ ImportCostPro.Application/ViewModels/OrderProductViewModels/OrderProductViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using ImportCostPro.Persistence.Enums;
 
 namespace ImportCostPro.Application.ViewModels.OrderProductViewModels
 {
-    internal class OrderProductViewModel
+    public class OrderProductViewModel
     {
+        public int Id { get; set; }
+        public int ImportOrderId { get; set; }
+        public int ProductId { get; set; }
+
+        [Display(Name = "Cód. Producto")]
+        public string ProductReferenceCode { get; set; } = null!;
+
+        [Display(Name = "Producto")]
+        public string ProductName { get; set; } = null!;
+
+        [Display(Name = "U.M.")]
+        public UnitOfMeasure UnitOfMeasure { get; set; }
+
+        [Display(Name = "Cantidad")]
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public decimal Quantity { get; set; }
+
+        [Display(Name = "FOB Unitario")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal UnitFobPrice { get; set; }
+
+        [Display(Name = "Subtotal FOB")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal SubtotalFob { get; set; }
+
+        [Display(Name = "Margen (%)")]
+        [DisplayFormat(DataFormatString = "{0:P2}")]
+        public decimal TargetProfitMargin { get; set; }
     }
 }
