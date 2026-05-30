3be3468d3f37d2f1bddea7ae221af3623e5b87f4
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-05-29 21:16:04 -0400

    Add appsettings.Development.json to .gitignore and update connection string in appsettings.json

--- ImportCostPro.Persistence/DependencyInjection.cs
+++ ImportCostPro.Persistence/DependencyInjection.cs
@@ -18,7 +18,7 @@ namespace ImportCostPro.Persistence
         {
             // Configuramos el DbContext para usar SQL Server con el connection string definido en appsettings.json
             services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                options.UseSqlServer(configuration.GetConnectionString("ImportCostDb"))
             );
 
             // Registramos el repo generico para que pueda ser inyectado en los otros repositorios
