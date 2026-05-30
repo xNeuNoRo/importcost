c4055fd4f97cb91ecdb4dbf53e2a54038824edb0
Ángel González Muñoz <119265574+xNeuNoRo@users.noreply.github.com>
2026-05-30 16:41:44 -0400

    Refactor terminology and improve UI text for import cost management
    - Updated text references from "Landed Cost" to "Liquidación de Costos" across multiple views to enhance clarity.
    - Improved user interface text for better understanding of import processes, including changes in descriptions and button labels.
    - Added a new Enum extension method to retrieve display names for enum values, improving code readability and maintainability.
    - Adjusted layout and styling for consistency and better user experience.

--- ImportCostPro.Persistence/Enums/OrderStatus.cs
+++ ImportCostPro.Persistence/Enums/OrderStatus.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ImportCostPro.Persistence.Enums
 {
     public enum OrderStatus
     {
+        [Display(Name = "Abierta")]
         Open = 1,
+
+        [Display(Name = "Liquidada")]
         Calculated = 2,
+
+        [Display(Name = "Cerrada")]
         Closed = 3,
+
+        [Display(Name = "Anulada")]
         Canceled = 4,
     }
 }
--- ImportCostPro.Persistence/Enums/UnitOfMeasure.cs
+++ ImportCostPro.Persistence/Enums/UnitOfMeasure.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ImportCostPro.Persistence.Enums
 {
     public enum UnitOfMeasure
     {
+        [Display(Name = "Unidad")]
         Unit = 1,
+
+        [Display(Name = "Caja")]
         Box = 2,
+
+        [Display(Name = "Paquete")]
         Pack = 3,
+
+        [Display(Name = "Docena")]
         Dozen = 4,
+
+        [Display(Name = "Galón")]
         Gallon = 5,
+
+        [Display(Name = "Metro")]
         Meter = 6,
     }
 }
